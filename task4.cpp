f2b1903cc00a52946066de11f67b5c7a79c22cc9
Toobatariq-cloud <noorulaint9@gmail.com>
2025-11-25 09:14:37 +0500

    Add files via upload

---
+++ task4.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"\n+-------------------------------+";
+	cout<<"\n|\tRollNo\t|\tMarks\t|";
+	cout<<"\n|\t109\t|\t79\t|";
+	cout<<"\n|\t112\t|\t96\t|";
+	cout<<"\n|\t114\t|\t94\t|";
+	cout<<"\n+-------------------------------+";
+	return 0;
+}
